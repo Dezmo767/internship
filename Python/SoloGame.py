87d4c516fe0d921d87466c362e09b5caf1f675ce
Dezmo767 <dezmo767@hotmail.com>
2018-02-26 14:45:57 +0100

    Update Python Games

---
+++ Python/SoloGame.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#Import same moduls
+import pygame
+import random
+import time
+
+pygame.init()
+
+#Display
+display = (800,800)
+pygame.display.set_mode(display)
+pygame.display.set_caption("Jump and Run")
+
+#Player
+player = pygame.image.load('car1.jpg')
+
+
+#Hindernisse
+
+
+
+#Keys
